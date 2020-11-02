a72e3aed71bd56c835221f5a865037721d7a3690
KazakovED <34635531+kazakovichna@users.noreply.github.com>
2020-11-02 14:50:01 +1000

    Add two layout files Make date alive Make dropdown alive \{*_*}/

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="sidebar">
+    <ul class="sidenav app-sidenav" :class="{open: value}">
+      <router-link
+        v-for="link in links"
+        :key="link.url"
+        tag="li"
+        active-class="active"
+        :to="link.url"
+        :exact="link.exact"
+      >
+        <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+      </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  props: ['value'],
+  data () {
+    return {
+      links: [
+        { title: 'Счет', url: '/', exact: true },
+        { title: 'История', url: '/history', exact: false },
+        { title: 'Планирование', url: '/planning', exact: false },
+        { title: 'Новая запись', url: '/record', exact: false },
+        { title: 'Категории', url: '/categories', exact: false }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
